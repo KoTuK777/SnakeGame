faa1f2164f12194cc64512756c01a344208d0b23
Anton Menchinskii <58192865+KoTuK777@users.noreply.github.com>
2020-01-28 11:25:21 +0200

    Update Source.cpp

--- SnakeGame/Source.cpp
+++ SnakeGame/Source.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <ctime>
-#include <vector>
 #include "Game.h"
-
 using namespace std;
+
+void hideCursor() {
+	CONSOLE_CURSOR_INFO structCursorInfo;
+	GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &structCursorInfo);
+	structCursorInfo.bVisible = FALSE;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &structCursorInfo);
+}
 
 int main() {
 	srand(time(nullptr));
+	hideCursor();
 
 	Game game;
 	game.play();
